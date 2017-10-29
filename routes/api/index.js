af37eb2512f2d62b1b167a4036cdb2c36405b496
Ifedayo Oluwadamilola <sureife@gmail.com>
2017-10-29 18:53:02 +0100

    [task] setup router for authController(login) and usersController(register,view)

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,14 +2,31 @@ const express = require("express");
 
 const router = express.Router();
 
+const passport = require("passport");
+
 const studentsController = require('../../controllers/students');
 
+const authController = require('../../controllers/auth');
+
+const usersController = require('../../controllers/users');
+
+const auth = passport.authenticate('jwt', { session: false });
+
 // Define Route Endpoints
-router.get('/students', studentsController.index);
-router.get('/students/:id', studentsController.show);
-router.post('/students', studentsController.create);
-router.put('/students/:id', studentsController.update);
-router.delete('/students/:id', studentsController.destroy);
+router.get('/students', auth , studentsController.index);
+router.get('/students/:id', auth ,studentsController.show);
+router.post('/students',  auth ,studentsController.create);
+router.put('/students/:id', auth,studentsController.update);
+router.delete('/students/:id', auth ,studentsController.destroy);
+
+router.post('/login', authController.login);
+
+router.get('/user',auth, usersController.show);
+
+router.post('/register', usersController.create);
+
+
+
 
 
 module.exports = router;
